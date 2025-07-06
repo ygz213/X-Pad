c8a94b0ae0d4e3a95ff0bcf8d83d394b72f099dd
ygz213 <yigitgurbuz0630@outlook.com>
2025-07-06 21:03:04 +0300

    Added basic interface

--- app/main.py
+++ app/main.py
@@ -9,14 +9,63 @@ class XPad(QtWidgets.QMainWindow):
         super().__init__()
         self.setWindowTitle('X-Pad')
         self.setWindowIcon(QtGui.QIcon(f'{path.join(path.dirname(argv[0]))}/icons/icon.png'))
-        self.showMaximized()
 
         self.menu_bar = QtWidgets.QMenuBar(self)
-        self.setMenuBar(self.menu_bar)
         self.menu_bar.addMenu('Settings')
+        self.setMenuBar(self.menu_bar)
         miscellaneous = self.menu_bar.addMenu('Miscellaneous')
         miscellaneous.addAction('GitHub repository')
         miscellaneous.addAction('License')
+        self.draw_widgets()
+
+
+    def draw_widgets(self):
+        main_frame = QtWidgets.QWidget(self)
+        self.setCentralWidget(main_frame)
+        layout = QtWidgets.QGridLayout(main_frame)
+        main_frame.setLayout(layout)
+
+        self.notes = QtWidgets.QTreeWidget(self)
+        self.notes.setHeaderHidden(True)
+        self.notes.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.CustomContextMenu)
+        self.notes.customContextMenuRequested.connect(self.show_context_menu)
+        layout.addWidget(self.notes)
+
+        button_layout = QtWidgets.QVBoxLayout()
+
+        self.new_note = QtWidgets.QPushButton('     New note...', self)
+        self.new_note.setIcon(QtGui.QIcon(f'{path.join(path.dirname(argv[0]))}/icons/new_note.png'))
+        self.new_note.setIconSize(QtCore.QSize(20, 20))
+        self.new_note.setFixedHeight(35)
+        self.new_note.setFixedWidth(125)
+        button_layout.addWidget(self.new_note)
+
+        self.new_folder = QtWidgets.QPushButton('     New folder...', self)
+        self.new_folder.setIcon(QtGui.QIcon(f'{path.join(path.dirname(argv[0]))}/icons/new_folder.png'))
+        self.new_folder.setIconSize(QtCore.QSize(20, 20))
+        self.new_folder.setFixedHeight(35)
+        self.new_folder.setFixedWidth(125)
+        button_layout.addWidget(self.new_folder)
+
+        buttons_frame = QtWidgets.QWidget(self)
+        buttons_frame.setLayout(button_layout)
+        layout.addWidget(buttons_frame, 0, 1, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+
+        self.showMaximized()
+
+
+
+    def show_context_menu(self, position):
+        context_menu = QtWidgets.QMenu(self)
+
+        edit_action = QtGui.QAction('Edit...', self)
+        #edit_action.triggered.connect()
+        context_menu.addAction(edit_action)
+
+        delete_action = QtGui.QAction('Remove...', self)
+        #delete_action.triggered.connect()
+        context_menu.addAction(delete_action)
+        context_menu.exec(self.notes.viewport().mapToGlobal(position))
 
 
 
